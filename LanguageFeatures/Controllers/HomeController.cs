5bf207e9bacd87e075daaf619e73927c0d3deae2
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 03:02:51 -0400

    Creating a Common Action Pattern in the HomeController.cs

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -5,32 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using LanguageFeatures.Models;
+using System.Linq;
 
 namespace LanguageFeatures.Controllers{
     public class HomeController : Controller{
 
-        public ViewResult Index() {
-
-            Product[] productArray = {
-            new Product {Name = "Kayak", Price = 275M},
-            new Product {Name = "Lifejacket", Price = 48.95M},
-            new Product {Name = "Soccer ball", Price = 19.50M},
-            new Product {Name = "Corner flag", Price = 34.95M}
-
-            };
-
-         
-
-            decimal priceFilterTotal = productArray
-            .Filter(p => (p?.Price ?? 0) >= 20)
-            .TotalPrices();
-            decimal nameFilterTotal = productArray
-            .Filter(p => p?.Name?[0] == 'S')
-            .TotalPrices();
-
-            return View("Index", new string[] {
-                $"Price Total: {priceFilterTotal:C2}",
-                $"Name Total: {nameFilterTotal:C2}" });
+        public ViewResult Index()
+        {
+            return View(Product.GetProducts().Select(p => p?.Name));
         }
     }
     
