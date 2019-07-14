6e90d238ae9040fbaa56f64643d88d86148fdb86
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 03:08:20 -0400

    Creating an Anonymous Type in the HomeController.cs

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,15 +8,20 @@ using LanguageFeatures.Models;
 using System.Linq;
 
 namespace LanguageFeatures.Controllers{
-    public class HomeController : Controller{
+    public class HomeController : Controller
+    {
 
         public ViewResult Index()
         {
-            var names = new[] { "Kayak", "Lifejacket", "Soccer ball" };
-            return View(names);
+            var products = new[] {
+            new { Name = "Kayak", Price = 275M },
+            new { Name = "Lifejacket", Price = 48.95M },
+            new { Name = "Soccer ball", Price = 19.50M },
+            new { Name = "Corner flag", Price = 34.95M }
+         };
+            return View(products.Select(p => p.Name));
         }
     }
-    
 }
 
     
