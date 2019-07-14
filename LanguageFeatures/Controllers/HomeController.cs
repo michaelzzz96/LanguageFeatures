bb2ca92485b7e20d599b6dd4984ef7d32f1f933c
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 01:15:16 -0400

    Initializing an Object in the HomeController.cs

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -10,17 +10,13 @@ namespace LanguageFeatures.Controllers{
     public class HomeController : Controller{
 
         public ViewResult Index() {
-            List<string> results = new List<string>();
-
-            foreach (Product p in Product.GetProducts()) {
-                string name = p?.Name ?? "<No Name>";
-                decimal? price = p?.Price ?? 0;
-                string relatedName = p?.Related?.Name ?? "<None>";
-                results.Add("$Name: {name}, Price: {price}, Related: {relatedName}");
+            string[] names = new string[3];
+            names[0] = "Bob";
+            names[1] = "Joe";
+            names[2] = "Alie";
+            return View("Index", names);
 
             }
-            return View(results);
-        }
         }
     }
 
