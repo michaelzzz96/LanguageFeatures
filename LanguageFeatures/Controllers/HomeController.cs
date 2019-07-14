21f3186d1e7b97520d05f4945041c6f96a8a2248
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 00:37:45 -0400

    Detecting null Values

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers{
     public class HomeController : Controller{
 
-        public ViewResult Index(){
-            return View(new string[] { "C#", "Language", "Features" });
+        public ViewResult Index() {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts()) {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                results.Add(string.Format("Name: {0), Price: {1}", name, price));
+
+
+            }
+            return View(results);
+        }
         }
     }
-}
+
