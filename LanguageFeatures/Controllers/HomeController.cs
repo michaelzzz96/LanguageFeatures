0bb9159f1084a84b19e13a1e1483999048398f67
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 00:47:48 -0400

    Fixing syntax error

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -16,8 +16,8 @@ namespace LanguageFeatures.Controllers{
                 string name = p?.Name;
                 decimal? price = p?.Price;
                 string relatedName = p?.Related?.Name;
-                results.Add(string.Format("Name: {0), Price: {1},Related: {2}", name, price, relatedName));
-
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",
+                name, price, relatedName));
 
             }
             return View(results);
