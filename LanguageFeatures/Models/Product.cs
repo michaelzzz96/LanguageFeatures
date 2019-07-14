c53d63b83f5e696af6730a68e9e346b7fad7d5de
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 00:58:11 -0400

    Using Auto-Implemented Property Initializers

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -7,13 +7,16 @@ namespace LanguageFeatures.Models{
     public class Product {
     
     public string Name { get; set; }
+    public string Category { get; set; } = "Watersports";
     public decimal? Price { get; set; }
     public Product Related { get; set; }
+
 
     public static Product[] GetProducts(){
 
             Product kayak = new Product{
                 Name = "Kayak",
+                Category = "Water Craft",
                 Price = 275M
             };
 
