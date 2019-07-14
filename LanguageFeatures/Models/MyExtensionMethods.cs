ca554e3050fe410348cf9ed5a043a5602b291800
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 02:21:45 -0400

    Applying an Extension Method in the HomeController.cs File in the Controllers Folder

--- LanguageFeatures/Models/MyExtensionMethods.cs
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 namespace LanguageFeatures.Models
 {
     public static class MyExtensionMethods {
+
         public static decimal TotalPrices(this ShoppingCart cartParam)
         {
             decimal total = 0;
