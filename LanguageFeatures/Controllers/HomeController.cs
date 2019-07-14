9c353a04a9ae442757d6ad94b7b01e17a04b821d
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 02:57:15 -0400

    Using a Function to Filter Objects in the HomeController.cs

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,12 +8,17 @@ using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
+   
     public class HomeController : Controller
     {
 
+        bool FilterByPrice(Product p)
+        {
+            return (p?.Price ?? 0) >= 20;
+        }
+
         public ViewResult Index()
         {
-            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
 
             Product[] productArray = {
             new Product {Name = "Kayak", Price = 275M},
@@ -23,13 +28,23 @@ namespace LanguageFeatures.Controllers
 
             };
 
-            decimal priceFilterTotal = productArray.FilterByPrice(20).TotalPrices();
-            decimal nameFilterTotal = productArray.FilterByName('S').TotalPrices();
+            Func<Product, bool> nameFilter = delegate (Product prod) {
+                return prod?.Name?[0] == 'S';
+            };
+
+            decimal priceFilterTotal = productArray
+            .Filter(FilterByPrice)
+            .TotalPrices();
+            decimal nameFilterTotal = productArray
+            .Filter(nameFilter)
+            .TotalPrices();
+
             return View("Index", new string[] {
-$"Price Total: {priceFilterTotal:C2}",
-$"Name Total: {nameFilterTotal:C2}" });
+                $"Price Total: {priceFilterTotal:C2}",
+                $"Name Total: {nameFilterTotal:C2}" });
         }
     }
+    
 }
 
     
