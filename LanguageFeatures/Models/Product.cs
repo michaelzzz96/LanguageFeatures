73f92a92a3353ff5ed5b77cc4993884442899399
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-13 23:55:51 -0400

    Added Content to the Product Class

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LanguageFeatures.Models
-{
-    public class Product
-    {
+namespace LanguageFeatures.Models{
+    public class Product {
+    
+    public string Name { get; set; }
+    public decimal? Price { get; set; }
+
+    public static Product[] GetProducts(){
+
+            Product kayak = new Product{
+                Name = "Kayak",
+                Price = 275M
+            };
+
+            Product lifeJacket = new Product{
+                Name = "Lifejacket",
+                Price = 48.95M
+            };
+
+            return new Product[] { kayak, lifeJacket, null };
+        }
+
     }
 }
