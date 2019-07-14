1eac266b7051e37210c140ab810f1d2e7fb1779f
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 03:18:59 -0400

    Using the async and await Keywords in the MyAsyncMethods.cs

--- LanguageFeatures/Models/MyAsyncMethods.cs
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -8,15 +8,14 @@ namespace LanguageFeatures.Models
 {
     public class MyAsyncMethods
     {
-        public static Task<long?> GetPageLength()
+        public async Task<long?> GetPageLength()
         {
             HttpClient client = new HttpClient();
 
-            var httpTask = client.GetAsync("http://apress.com");
-            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
-            {
-                return antecedent.Result.Content.Headers.ContentLength;
-            });
+            var httpTask =  await client.GetAsync("http://apress.com");
+
+            return httpTask.Content.Headers.ContentLength;
+            }
         }
     }
-}
+
