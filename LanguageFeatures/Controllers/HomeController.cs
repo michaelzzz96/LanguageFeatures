50d7a1e2457fb92e12463db13415e4e5e70091fe
michaelzzz96 <michaelzzz1996@gmail.com>
2019-07-14 00:09:51 -0400

    Added Contents to HomeController

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
-namespace LanguageFeatures.Controllers
-{
-    public class HomeController
-    {
+namespace LanguageFeatures.Controllers{
+    public class HomeController : Controller{
+
+        public ViewResult Index(){
+            return View(new string[] { "C#", "Language", "Features" });
+        }
     }
 }
